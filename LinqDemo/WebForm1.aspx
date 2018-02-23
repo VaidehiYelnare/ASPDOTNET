72ae2c7ae9e7c4f3b691d8dc5ca9af1ce18b70dd
VaidehiYelnare <vryelnare@gmail.com>
2018-02-23 09:29:33 +0530

    LinqFormDemo

---
+++ LinqDemo/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LinqQueryDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AutoGenerateColumns ="false">
+         <Columns>
+            <asp:BoundField DataField="ID" HeaderText="ID"/>
+            <asp:BoundField DataField="FirstName" HeaderText="FirstName"/>
+            <asp:BoundField DataField="LastName" HeaderText="LastName"/>
+            <asp:BoundField DataField="Gender" HeaderText="Gender"/>
+            <asp:BoundField DataField="DeptID" HeaderText="DeptID"/>
+            <asp:BoundField DataField="Salary" HeaderText="Salary"/>
+         </Columns>
+        </asp:GridView>
+    
+        <br />
+    
+    </div>
+        <asp:Button ID="btnShow" runat="server" OnClick="btnShow_Click" Text="Show" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+    &nbsp;&nbsp;
+        <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+    </form>
+</body>
+</html>
---
+++ LinqDemo/WebForm1.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqQueryDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private object dbContext;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //GetData();
+        }
+
+        private void GetData()
+        {
+            SampleDataContext dbContext = new SampleDataContext();
+            GridView1.DataSource = dbContext.Employees;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          //  GetData();
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee obj = new Employee
+                {
+                    ID         =  6,
+                    FirstName  = "Shravani",
+                    LastName   = "Deshpande",
+                    Gender     = "Female", 
+                    DeptID     = 3,       
+                    Salary     = 66000
+                };
+                dbContext.Employees.InsertOnSubmit(obj);
+                dbContext.SubmitChanges();
+
+            }//end of using
+            GetData();
+        }//end of btnInsert
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee obj1 = dbContext.Employees.SingleOrDefault(update => update.ID == 5);
+                obj1.Salary = 65500;
+                dbContext.SubmitChanges();
+            }//end of using
+            GetData();
+        }//end of btnUpdate
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee obj2 = dbContext.Employees.SingleOrDefault(delete => delete.ID == 5);
+                dbContext.Employees.DeleteOnSubmit(obj2);
+                dbContext.SubmitChanges();
+            }//end of using
+            GetData();
+        }
+    }
+}
